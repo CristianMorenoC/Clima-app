d735daa685b496804c025fbb35e76c0feb9df996
CristianMorenoC <88695275+CristianMorenoC@users.noreply.github.com>
2021-11-22 22:28:13 -0500

    error404

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Header from './componentes/Header';
 import Inicio from './Rutas/Inicio/Inicio';
 import Detalles from './Rutas/informacion/Detalles';
-import Error404 from './componentes/error404/Error404';
 
 //context
 import { CardProvider } from './context/CardContext';
@@ -18,7 +17,6 @@ function App() {
                 <Switch>
                   <Route path="/" exact={true} component={Inicio} />
                   <Route path="/information" component={Detalles} />
-                  <Route path="*" component={Error404} />
                 </Switch>
           </div>
       </BrowserRouter>
--- src/componentes/error404/Error404.jsx
+++ src/componentes/error404/Error404.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Error404 = () => {
     return ( 
-        <>
-        <h1>Pagina no encontrada</h1>
-        <p>La ciudad que esta buscando, no se encuentra</p>
-        <Link to="/">Ir a Inicio</Link>
-        </>
+        <div className="flex flex-wrap justify-center items-center flex-col">
+        <h1 className="text-red-600 text-4xl font-extrabold text-center">Error 404</h1>
+        <p className="m-8">La ciudad que esta buscando, no se encuentra</p>
+        <Link to="/information" className="px-4 py-1 rounded bg-blue-400 hover:bg-blue-600">Ir a Inicio</Link>
+        </div>
      );
 }
  
